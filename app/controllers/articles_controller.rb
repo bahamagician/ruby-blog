03954a98cfc0b414b729d8c1f85f222c203ea9fe
Chris Thronebury <cthronebury@gmail.com>
2016-02-20 23:16:38 -0500

    Update article create method to use current user

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,10 +19,8 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    debugger
-    # render plain: params[:article].inspect
     @article = Article.new(article_params)
-    @article.user= User.first
+    @article.user= current_user
     if @article.save
       flash[:success] = "Article was successfully created!!!"
       redirect_to article_path(@article)
